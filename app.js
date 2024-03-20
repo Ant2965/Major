059b3368800827678c1ddd3405ffa8e7904fb711
Ant2965 <antrikshbahri@gmail.com>
2024-03-20 20:00:19 +0530

    EJS Mates and starting back

--- app.js
+++ app.js
@@ -1,103 +1,94 @@
-const express = require("express")
-const mongoose = require("mongoose")
+const express = require("express");
 const app = express();
-const Listing = require ("./models/listing.js")
-const path =require("path")
-app.set("view engine","ejs")
-app.set("views",path.join(__dirname,"views"))
-app.use(express.urlencoded({extended: true}))
-const url = "mongodb://127.0.0.1:27017/wanderlust"
+const mongoose = require("mongoose");
+const Listing = require("./models/listing.js");
+const path = require("path");
+const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate")
+const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
-
-main().then(() => {
-    console.log("connect to db")
-})
-    .catch((err) => {
-        console.log(err)
-    })
+main()
+  .then(() => {
+    console.log("connected to DB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 async function main() {
-    mongoose.connect(url)
+  await mongoose.connect(MONGO_URL);
 }
 
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+app.engine('ejs',ejsMate)
 app.get("/", (req, res) => {
-    res.send("Hi i am root")
-})
+  res.send("Hi, I am root");
+});
 
+//Index Route
+app.get("/listings", async (req, res) => {
+  const allListings = await Listing.find({});
+  res.render("listings/index.ejs", { allListings });
+});
 
-// app.get("/testlisting", async (req, res) => {
-//     let samplelisting = new Listing({
-//         title: "My new villa",
-//         description: "by rhe beach",
-//         price: 21000,
-//         location: "calcultta",
-//         country: "India"
+//New Route
+app.get("/listings/new", (req, res) => {
+  res.render("listings/new.ejs");
+});
 
-//     })
-//     await samplelisting.save();
-//     console.log("sample data saved")
-//     res.send("success test")
-// })
+//Show Route
+app.get("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  const listing = await Listing.findById(id);
+  res.render("listings/show.ejs", { listing });
+});
 
+//Create Route
+app.post("/listings", async (req, res) => {
+  const newListing = new Listing(req.body.listing);
+  await newListing.save();
+  res.redirect("/listings");
+});
 
+//Edit Route
+app.get("/listings/:id/edit", async (req, res) => {
+  let { id } = req.params;
+  const listing = await Listing.findById(id);
+  res.render("listings/edit.ejs", { listing });
+});
 
-app.get("/listings",async (req,res)=>{
-  const alllisting= await Listing.find({})
+//Update Route
+app.put("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+  res.redirect(`/listings/${id}`);
+});
 
-  res.render("listings/index.ejs",{alllisting});
-      
-})
+//Delete Route
+app.delete("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  let deletedListing = await Listing.findByIdAndDelete(id);
+  console.log(deletedListing);
+  res.redirect("/listings");
+});
 
+// app.get("/testListing", async (req, res) => {
+//   let sampleListing = new Listing({
+//     title: "My New Villa",
+//     description: "By the beach",
+//     price: 1200,
+//     location: "Calangute, Goa",
+//     country: "India",
+//   });
 
+//   await sampleListing.save();
+//   console.log("sample was saved");
+//   res.send("successful testing");
+// });
 
-
-
-
-
-// new listing route
-app.get("/listings/new", (req,res)=>{
-   res.render("listings/new.ejs")
-})
-
-
-app.post("/listings",async (req,res)=>{
-
-
-   const newlisting= new Listing(req.body.listing);    
-   await newlisting.save();
-   res.redirect("/listings")
-
-
-})
-
-
-
-
-
-
-app.get("/listings/:id",async (req,res)=>{
-
-    let{id}=req.params;
-    const listing= await Listing.findById(id);
-    res.render("listings/show.ejs",{listing});
-
-
-})
-
-
-// show route
-
-
-
-
-
-
-
-
-
-
-
-
-app.listen(8069, () => {
-    console.log("server on on 8069")
-})
+app.listen(6080, () => {
+  console.log("server is listening to port 8080");
+});
