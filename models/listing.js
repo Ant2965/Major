997997710c241c68d3175f9a44abc002acfde115
Ant2965 <antrikshbahri@gmail.com>
2024-03-24 05:46:16 +0530

    Done withEdit page  and Listing pages

--- models/listing.js
+++ models/listing.js
@@ -8,12 +8,10 @@ const listingSchema = new Schema({
   },
   description: String,
   image: {
-    filename: String,
-    url: {
-      type: String,
-      default:
-        "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdvYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=6",
-    },
+    type: String,
+
+    set: (v) => v === "" ? "https://unsplash.com/photos/man-sitting-on-rock-surrounded-by-water--Q_t4SCN8c4" : v,
+
   },
   price: Number,
   location: String,
